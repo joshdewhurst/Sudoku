58963c00e6ddfeeb1edecca1e36a74a98936ce1f
joshdewhurst <joshuardewhurst@gmail.com>
2023-04-22 12:57:28 -0700

    put board in console

--- main.cpp
+++ main.cpp
@@ -15,3 +15,19 @@ const int EMPTY = 0;
 
 // Define the board type
 typedef vector<vector<int>> Board;
+
+// Print the Sudoku board to the console
+void print_board(const Board& board) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            cout << board[i][j] << " ";
+            if ((j + 1) % SUBGRID_SIZE == 0 && j < BOARD_SIZE - 1) {
+                cout << "| ";
+            }
+        }
+        cout << endl;
+        if ((i + 1) % SUBGRID_SIZE == 0 && i < BOARD_SIZE - 1) {
+            cout << "------+-------+------" << endl;
+        }
+    }
+}
