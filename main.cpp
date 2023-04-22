5d72349837ca2464fe560a4791953978deb3c9c6
joshdewhurst <joshuardewhurst@gmail.com>
2023-04-22 12:56:41 -0700

    defining sudoku board

--- main.cpp
+++ main.cpp
@@ -5,3 +5,13 @@
 #include <chrono>
 
 using namespace std;
+
+// Define the size of the Sudoku board
+const int BOARD_SIZE = 9;
+const int SUBGRID_SIZE = 3;
+
+// Define the empty cell value
+const int EMPTY = 0;
+
+// Define the board type
+typedef vector<vector<int>> Board;
