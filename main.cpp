d12b98f335d97f3a56115503e885a3139e1912ae
joshdewhurst <joshuardewhurst@gmail.com>
2023-04-22 12:59:44 -0700

    solving the game

--- main.cpp
+++ main.cpp
@@ -53,3 +53,40 @@ bool is_valid(const Board& board, int row, int col, int value) {
     // If all checks passed, the value is valid
     return true;
 }
+
+// Find the next empty cell on the board
+bool find_empty_cell(const Board& board, int& row, int& col) {
+    for (row = 0; row < BOARD_SIZE; row++) {
+        for (col = 0; col < BOARD_SIZE; col++) {
+            if (board[row][col] == EMPTY) {
+                return true;
+            }
+        }
+    }
+    // If no empty cell was found, the board is full
+    return false;
+}
+
+// Solve the Sudoku board using backtracking
+bool solve_board(Board& board) {
+    int row, col;
+    // Find the next empty cell
+    if (!find_empty_cell(board, row, col)) {
+        return true; // The board is full and solved
+    }
+    // Try values for the empty cell and backtrack if they are not valid
+    for (int value = 1; value <= BOARD_SIZE; value++) {
+        if (is_valid(board, row, col, value)) {
+            board[row][col] = value;
+            if (solve_board(board)) {
+                return true;
+            }
+            board[row][col] = EMPTY;
+        }
+    }
+    // If no valid value was found, backtrack
+    return false;
+}
+
+// Generate a random Sudoku board
+void generate_board(Board& board)
